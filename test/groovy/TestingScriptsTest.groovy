75e1ddd7b0eaa01e4d109fc487f8cf9a555867be
JC Francisco <jc.francisco17@gmail.com>
2022-04-05 09:04:04 +0800

    JDK 17 requires at least groovy 3.0.8

---
+++ test/groovy/TestingScriptsTest.groovy
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test
+
+class TestingScriptsTest {
+
+    @Test
+    void exampleTest() {
+        fail()
+    }
+
+}

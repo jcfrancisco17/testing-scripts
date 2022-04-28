4146a3448218e990870d0dd067c5f8acc39f44b6
JC Francisco <jc.francisco17@gmail.com>
2022-04-28 10:45:15 +0800

    Bit of cleanup. Added simpleTest
    - Did a bit of cleanup for readability.
    - Added `simpleTest` test case to demonstrate how to execute a bash script with groovy.
    - Used `process.onExit().get().exitValue()` instead of just `process.exitValue()`. The former produces consistent results, the latter doesn't.

--- scripts/bash-script-that-inserts-data.sh
+++ scripts/bash-script-that-inserts-data.sh
@@ -6,4 +6,8 @@ DB_PORT=$3
 DB_NAME=$4
 DB_PASSWORD=$5
 
+echo "Hello, I'm a script"
+
 PGPASSWORD=$DB_PASSWORD psql -qtAX -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" "$DB_NAME" -c "INSERT INTO items (name) VALUES ('item 1');"
+
+echo "Script done"
--- test/groovy/TestingScriptsTest.groovy
+++ test/groovy/TestingScriptsTest.groovy
@@ -1,6 +1,8 @@
 import groovy.sql.Sql
-import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.postgresql.ds.PGSimpleDataSource
 import org.testcontainers.containers.JdbcDatabaseContainer
 import org.testcontainers.containers.PostgreSQLContainer
@@ -8,61 +10,70 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
-import static org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import static org.junit.jupiter.api.Assertions.assertEquals
 
 @Testcontainers
 class TestingScriptsTest {
 
-    private static String databaseName
-    private static String username
-    private static String password
-    private static String host
-    private static int port
+    private static final String POSTGRES_DOCKER_IMAGE = "postgres:11.6-alpine"
+    private static final String DB_NAME = "test_db"
+    private static final String DB_USERNAME = "user"
+    private static final String DB_PASSWORD = "pass"
+    private static final String DB_HOST = "localhost"
+    private static final int POSTGRES_PORT = 5432
+    private static final String INIT_SCRIPT_PATH = "schema.sql"
+    private static int mapperPort
     private static Sql sql
 
     @Container
     private static final JdbcDatabaseContainer POSTGRESQL_CONTAINER = new PostgreSQLContainer(
-            DockerImageName.parse('postgres:11.6-alpine'))
-            .withDatabaseName('test_db')
-            .withUsername('user')
-            .withPassword('pass')
-            .withInitScript('schema.sql')
+            DockerImageName.parse(POSTGRES_DOCKER_IMAGE))
+            .withDatabaseName(DB_NAME)
+            .withUsername(DB_USERNAME)
+            .withPassword(DB_PASSWORD)
+            .withInitScript(INIT_SCRIPT_PATH)
 
     @BeforeAll
     static void beforeAll() {
         assert POSTGRESQL_CONTAINER.running
 
-        databaseName = POSTGRESQL_CONTAINER.databaseName
-        username = POSTGRESQL_CONTAINER.username
-        password = POSTGRESQL_CONTAINER.password
-        host = POSTGRESQL_CONTAINER.host
-        port = POSTGRESQL_CONTAINER.getMappedPort(5432) // this is to acquire the random port that testcontainer uses
+        // this is to acquire the random port that testcontainer uses
+        mapperPort = POSTGRESQL_CONTAINER.getMappedPort(POSTGRES_PORT)
 
+        // instantiate the groovy SQL object.
         sql = new Sql(
                 new PGSimpleDataSource(
-                        serverName: host,
-                        portNumber: port,
-                        databaseName: databaseName,
-                        user: username,
-                        password: password)
+                        serverName: DB_HOST,
+                        databaseName: DB_NAME,
+                        user: DB_USERNAME,
+                        password: DB_PASSWORD,
+                        portNumber: mapperPort)
         )
     }
 
-    @AfterAll
-    static void afterAll() {
+    @AfterEach
+    void afterEach() {
         sql.execute('TRUNCATE items')
+    }
+
+    @Test
+    @DisplayName("Assert bash script can be executed")
+    void simpleTest() {
+        Process process = "sh ./scripts/simple-bash-script.sh".execute()
+
+        println(process.text) // prints standard output
+        assertEquals(0, process.onExit().get().exitValue(), "Exit value should be zero.")
     }
 
     @SuppressWarnings('GroovyAssignabilityCheck')
     @Test
     @DisplayName("Assert script inserts data")
     void insertTest() {
-        Process process = "sh ./scripts/bash-script-that-inserts-data.sh $username $host $port $databaseName $password".execute()
-        process.consumeProcessErrorStream(new StringBuffer()) //need this here or else process will not exit
+        Process process = "sh ./scripts/bash-script-that-inserts-data.sh $DB_USERNAME $DB_HOST $mapperPort $DB_NAME $DB_PASSWORD".execute()
 
-        assertEquals(0, process.exitValue(), "Exit value should be zero.")
+        println(process.text) //prints standard output
+        assertEquals(0, process.onExit().get().exitValue(), "Exit value should be zero.")
         assertEquals(1, sql.firstRow('SELECT COUNT(id) as num FROM items').num)
     }
+
 }
